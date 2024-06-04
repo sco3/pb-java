5260ef9ec936ae95b745f35f8b0119dc629cea37
sco3 <dzakarov@gmail.com>
2024-06-04 16:59:11 +0100

    save data to file

--- src/main/java/sco3/TestPb.java
+++ src/main/java/sco3/TestPb.java
@@ -1,10 +1,12 @@
 package sco3;
 
 import static java.lang.System.out;
+import static java.nio.file.Files.readAllBytes;
+import static java.nio.file.Paths.get;
+import static sco3.Test.Hello.newBuilder;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import sco3.Test.Hello;
 
@@ -12,10 +14,22 @@ public class TestPb {
 	private static final String PB_FILE = "/tmp/a-hello.pb";
 
 	public static void main(String[] argv) throws IOException {
-		Hello aHello = Hello.newBuilder().setName("asdf").build();
-		out.println("Data: " + aHello);
+		writeToFile();
+		readFromFile();
+	}
+
+	private static void readFromFile() throws IOException {
+		byte[] b = readAllBytes(get(PB_FILE));
+		Hello aHello = Hello.parseFrom(b);
+		out.println("Data from file: " + PB_FILE + " -> [\n" + aHello + "]");
+	}
+
+	private static void writeToFile() throws IOException {
+		Hello aHello = newBuilder().setName("asdf").build();
+		out.println("Data to save -> [\n" + aHello + "]");
+
 		byte[] b = aHello.toByteArray();
-		Files.write(Paths.get(PB_FILE), b);
+		Files.write(get(PB_FILE), b);
 		out.println("Binary was saved to " + PB_FILE);
 	}
 }
