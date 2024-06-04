eb01898f7b9ad4dfbe9dcb2f19d0912047053ef0
sco3 <dzakarov@gmail.com>
2024-06-04 16:47:39 +0100

    test data message

--- src/main/java/sco3/TestPb.java
+++ src/main/java/sco3/TestPb.java
@@ -1,7 +1,18 @@
 package sco3;
 
+import static java.lang.System.out;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import sco3.Test.Hello;
+
 public class TestPb {
-	public static void main() {
-		System.out.println("Aha");
+	public static void main(String[] argv) throws IOException {
+		Hello aHello = Hello.newBuilder().setName("asdf").build();
+		out.println(aHello);
+		byte[] b = aHello.toByteArray();
+		Files.write(Paths.get("/tmp/a-hello.pb"), b);
 	}
 }
