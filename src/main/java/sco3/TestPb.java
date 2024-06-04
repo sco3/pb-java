64d80629564e72825ac7bba334df95266f43effe
sco3 <dzakarov@gmail.com>
2024-06-04 16:53:05 +0100

    save data to file

--- src/main/java/sco3/TestPb.java
+++ src/main/java/sco3/TestPb.java
@@ -9,10 +9,13 @@ import java.nio.file.Paths;
 import sco3.Test.Hello;
 
 public class TestPb {
+	private static final String PB_FILE = "/tmp/a-hello.pb";
+
 	public static void main(String[] argv) throws IOException {
 		Hello aHello = Hello.newBuilder().setName("asdf").build();
-		out.println(aHello);
+		out.println("Data: " + aHello);
 		byte[] b = aHello.toByteArray();
-		Files.write(Paths.get("/tmp/a-hello.pb"), b);
+		Files.write(Paths.get(PB_FILE), b);
+		out.println("Binary was saved to " + PB_FILE);
 	}
 }
