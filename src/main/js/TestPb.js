fdd6fa9e8a8e18089babe20863fe07ca25f5b076
sco3 <dzakarov@gmail.com>
2024-06-05 10:16:18 +0100

    nodejs test

---
+++ src/main/js/TestPb.js
@@ -0,0 +1,10 @@
+
+var goog = require('google-protobuf')
+const GEN_DIR = '../../../build/generated/source/proto/main'
+var testProto = require(GEN_DIR + '/js/test_pb');
+
+function main() {
+	console.log("Aha");
+}
+
+main();
