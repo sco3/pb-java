961c20bd497394844f44a6b3fd16f17df0a6d047
sco3 <dzakarov@gmail.com>
2024-06-05 13:06:57 +0100

    js

--- src/main/js/TestPb.js
+++ src/main/js/TestPb.js
@@ -1,13 +1,30 @@
 
+
 var goog = require('google-protobuf')
-//const GEN_DIR = '../../../build/generated/source/proto/main'
 var testProto = require('./test_pb');
 
+const A_HELLO_PB = '/tmp/a-hello.pb'
+
 function main() {
-	console.log("Aha");
-	var h = new proto.sco3.Hello();
-	h.setName("aha")
-	console.log(h);
+	//var h = new proto.sco3.Hello();
+	//h.setName("aha")
+	const fs = require('fs');
+	if (fs.existsSync(A_HELLO_PB)) {
+		var stats = fs.statSync(A_HELLO_PB);
+		fs.open(A_HELLO_PB, 'r', function(status, fd) {
+			if (status) {
+				console.log(status.message);
+				return;
+			}
+			var buffer = Buffer.alloc(stats.size);
+			fs.read(fd, buffer, 0, stats.size, 0, function(err, num) {
+				var h = proto.sco3.Hello.deserializeBinary(buffer)
+				console.log(h);
+			});
+		});
+	}
 }
 
-main();
+if (require.main === module) {
+	main();
+}
