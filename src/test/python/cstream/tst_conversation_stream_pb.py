318defbd995fa8fe122266af94569f76366b08e4
sco3 <dzakarov@gmail.com>
2024-08-16 09:21:33 +0100

    stubs

---
+++ src/test/python/cstream/tst_conversation_stream_pb.py
@@ -0,0 +1,12 @@
+import time
+from typing import ClassVar
+import unittest
+
+
+class TestSerDe(unittest.TestCase):
+    CSTREAM_BIN: ClassVar[str] = "/tmp/cstream.bin"
+    file_result: bool = False
+    buf: bytes = b""
+
+    def test_01_create_conversation_stream_message(self) -> None:
+        pass
